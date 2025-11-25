27b2cb26d92327d15a498ad48c61933ef7b99347
fvishw <fvishw@gmail.com>
2025-11-25 15:55:58 +0530

    refactor: Add MongoDB configuration and update scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import app from "./app.js";
 
+import { connectDB } from "./config/db.js";
 const PORT = process.env.PORT || 5000;
-
+console.log(PORT);
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
