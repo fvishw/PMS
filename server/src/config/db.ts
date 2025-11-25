54fba2f9ac5c5b64af7ffa189809acfc2424433e
fvishw <fvishw@gmail.com>
2025-11-25 15:56:31 +0530

    refactor: Remove console log for MongoDB URI

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/";
-console.log(MONGODB_URI);
 export const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI);
