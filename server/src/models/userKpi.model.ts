7884b0178a155a2a24b73a7ee87485ec800c79d0
fvishw <fvishw@gmail.com>
2025-12-04 12:59:38 +0530

    feat: Implement UserKpi model with schema and interfaces for KPI tracking

---
+++ server/src/models/userKpi.model.ts
@@ -0,0 +1,44 @@
+import { Schema, model, Types } from "mongoose";
+import type { IKpi } from "./masterKpi.model.ts";
+
+interface IUserKpi extends IKpi {
+  user: Types.ObjectId;
+  managerId: Types.ObjectId;
+}
+
+const UserKpiSchema = new Schema<IUserKpi>(
+  {
+    user: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    designation: {
+      type: Types.ObjectId,
+      ref: "Designation",
+      required: true,
+    },
+    criteria: [
+      {
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        weight: { type: Number, required: true },
+        selfScore: { type: Number },
+        selfComments: { type: String },
+        managerScore: { type: Number },
+        managerComments: { type: String },
+      },
+    ],
+    managerId: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isKpiLocked: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const UserKpi = model<IUserKpi>("UserKpi", UserKpiSchema);
+
+export { UserKpi };
