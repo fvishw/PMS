0b17c1f2961745c49d87800b35479c0a6fa00fc2
fvishw <fvishw@gmail.com>
2025-12-04 12:56:34 +0530

    fix: Update user model to use Types

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Document } from "mongoose";
+import { Schema, model, Document, Types } from "mongoose";
 import bcrypt from "bcrypt";
 import AuthService from "../utils/AuthService.ts";
 
@@ -7,7 +7,7 @@ export interface IUser extends Document {
   email: string;
   password: string;
   role: "admin" | "user" | "manager";
-  designation: Schema.Types.ObjectId;
+  designation: Types.ObjectId;
   createdAt: Date;
   updatedAt: Date;
   isSignUpComplete?: boolean;
@@ -33,7 +33,7 @@ const UserSchema = new Schema<IUser>(
     designation: {
       type: Schema.Types.ObjectId,
       ref: "Designation",
-      required: true,
+      // required: true,
     },
     isSignUpComplete: { type: Boolean, default: false },
     refreshToken: { type: String, default: "" },
