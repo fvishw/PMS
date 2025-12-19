325d0e94bc71530ec76bbf5a827b7faa6867dccf
fvishw <fvishw@gmail.com>
2025-12-19 14:18:47 +0530

    feat: add criteria options for CheckIn feedback

---
+++ client/src/components/checkins/options.ts
@@ -0,0 +1,26 @@
+import IOption from "@/types/option";
+
+const criteriaOptions: IOption[] = [
+  {
+    label: "Strongly Disagree",
+    value: "strongly-disagree",
+  },
+  {
+    label: "Disagree",
+    value: "disagree",
+  },
+  {
+    label: "Neutral",
+    value: "neutral",
+  },
+  {
+    label: "Agree",
+    value: "agree",
+  },
+  {
+    label: "Strongly Agree",
+    value: "strongly-agree",
+  },
+];
+
+export default criteriaOptions;
