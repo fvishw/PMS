4665378ca8947f0ef3970273b7de9f08626d9026
fvishw <fvishw@gmail.com>
2026-04-14 11:42:18 +0530

    feat: remove terms of service and privacy policy descriptions from login, signup, and reset password forms

--- client/src/components/auth/login-form.tsx
+++ client/src/components/auth/login-form.tsx
@@ -163,10 +163,6 @@ export function LoginForm({
           </div>
         </CardContent>
       </Card>
-      <FieldDescription className="px-6 text-center">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </FieldDescription>
     </div>
   );
 }
--- server/src/routes/checkIns.routes.ts
+++ server/src/routes/checkIns.routes.ts
@@ -24,7 +24,7 @@ router.get("/", authMiddleware(["employee", "manager", "admin"]), getCheckIns);
 
 router.get(
   "/past-checkins",
-  authMiddleware(["employee", "admin"]),
+  authMiddleware(["employee", "manager", "admin"]),
   getPastCheckIns,
 );
 
